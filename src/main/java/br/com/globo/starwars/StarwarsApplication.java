0c405722c113ad943cb3d1f754b066b21b37f7af
Douglas Rocha <douglas.vieira@radix.com.br>
2020-10-07 21:32:13 -0300

    Criando Bean de RestTemplate com header de contentType application/json definido por padrão

--- src/main/java/br/com/globo/starwars/StarwarsApplication.java
+++ src/main/java/br/com/globo/starwars/StarwarsApplication.java
@@ -1,7 +1,15 @@
 package br.com.globo.starwars;
 
+import br.com.globo.starwars.interceptor.RestTemplateHeaderModifierInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class StarwarsApplication {
@@ -10,4 +18,19 @@ public class StarwarsApplication {
 		SpringApplication.run(StarwarsApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate() {
+		RestTemplate restTemplate = new RestTemplate();
+
+		List<ClientHttpRequestInterceptor> interceptors = restTemplate.getInterceptors();
+
+		if (CollectionUtils.isEmpty(interceptors)) {
+			interceptors = new ArrayList<>();
+		}
+
+		interceptors.add(new RestTemplateHeaderModifierInterceptor());
+		restTemplate.setInterceptors(interceptors);
+
+		return restTemplate;
+	}
 }
