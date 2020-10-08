811179468f1a8f343edcbbb8825c1a885f2e9f9a
Douglas Rocha <douglas.vieira@radix.com.br>
2020-10-08 14:10:42 -0300

    Criando handler de erro padrão para exceções da API

---
+++ src/main/java/br/com/globo/starwars/exception/ApiRequestException.java
@@ -0,0 +1,11 @@
+package br.com.globo.starwars.exception;
+
+public class ApiRequestException extends RuntimeException {
+    public ApiRequestException(String message) {
+        super(message);
+    }
+
+    public ApiRequestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/main/java/br/com/globo/starwars/exception/ApiRequestHandler.java
@@ -0,0 +1,22 @@
+package br.com.globo.starwars.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+@ControllerAdvice
+public class ApiRequestHandler {
+
+    @ExceptionHandler(value = {ApiRequestException.class})
+    public ResponseEntity<Object> handleApiRequestException(ApiRequestException exception) {
+        ApiException apiException = new ApiException(
+            exception.getMessage(), HttpStatus.BAD_REQUEST, ZonedDateTime.now(ZoneId.of("Z"))
+        );
+
+        return new ResponseEntity<>(apiException, HttpStatus.BAD_REQUEST);
+    }
+}
