c859013b785d965ed1e5361aaa0097fd2f94f6d2
Douglas Rocha <douglas.vieira@radix.com.br>
2020-10-07 21:33:56 -0300

    Adicionando endpoint para recuperar a lista de todos os filmes

--- src/main/java/br/com/globo/starwars/controller/StarWarsController.java
+++ src/main/java/br/com/globo/starwars/controller/StarWarsController.java
@@ -2,11 +2,14 @@ package br.com.globo.starwars.controller;
 
 import br.com.globo.starwars.model.Movie;
 import br.com.globo.starwars.service.StarWarsService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("starwars")
@@ -15,8 +18,11 @@ public class StarWarsController {
     @Autowired
     private StarWarsService starWarsService;
 
-    @GetMapping(value = "movies/{id}")
-    public Movie getMovieById(@PathVariable("id") Long id) {
+    @GetMapping("movies/{id}")
+    public Movie getMovieById(@PathVariable("id") Long id) throws JsonProcessingException {
         return starWarsService.getMovieById(id);
     }
+
+    @GetMapping("movies")
+    public List<Movie> getAllMovies() { return starWarsService.getAllMovies(); }
 }
--- src/main/java/br/com/globo/starwars/service/StarWarsService.java
+++ src/main/java/br/com/globo/starwars/service/StarWarsService.java
@@ -1,31 +1,61 @@
 package br.com.globo.starwars.service;
 
 import br.com.globo.starwars.model.Movie;
+import br.com.globo.starwars.model.MovieWrapper;
+import br.com.globo.starwars.model.People;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.text.MessageFormat;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 @Service
 public class StarWarsService {
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     private static final String BASE_URL = "https://swapi.dev/api/";
     private static final String URL_MOVIES = BASE_URL + "films/";
     private static final String URL_MOVIE_BY_ID = URL_MOVIES + "{0}/";
 
-    public Movie getMovieById(Long id) {
-        RestTemplate restTemplate = new RestTemplate();
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-
-        HttpEntity<String> entity = new HttpEntity<>(headers);
-
-        return restTemplate.exchange(MessageFormat.format(URL_MOVIE_BY_ID, id), HttpMethod.GET, entity, Movie.class).getBody();
+    public List<Movie> getAllMovies() {
+        return restTemplate.getForObject(URL_MOVIES, MovieWrapper.class).getMovies();
     }
+
+    public Movie getMovieById(Long id) throws JsonProcessingException {
+        Movie movie = restTemplate.getForObject(MessageFormat.format(URL_MOVIE_BY_ID, id), Movie.class);
+        movie.setId(id);
+        movie.setCast(createListCharactersForMovie(movie));
+
+        return movie;
+    }
+
+    private List<String> createListCharactersForMovie(Movie movie) {
+        List<String> cast = new ArrayList<>();
+
+        if (movie.getCharacters() != null && !movie.getCharacters().isEmpty()) {
+            for (String people : movie.getCharacters()) {
+                String url = people.replaceAll("http", "https");
+                ResponseEntity<People> response = restTemplate.exchange(url, HttpMethod.GET, null, People.class);
+
+                if (response.getBody() != null) {
+                    cast.add(response.getBody().getName());
+                }
+            }
+        }
+        return cast;
+    }
+
 }
