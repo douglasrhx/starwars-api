2848da85ffb5a8972e8b6389353a7c86a10a60ca
Douglas Rocha <douglas.vieira@radix.com.br>
2020-10-08 01:22:54 -0300

    Criando cache no redis das chamadas aos métodos de recuperar filmes

--- src/main/java/br/com/globo/starwars/service/MovieService.java
+++ src/main/java/br/com/globo/starwars/service/MovieService.java
@@ -4,36 +4,39 @@ import br.com.globo.starwars.model.Movie;
 import br.com.globo.starwars.model.MovieWrapper;
 import br.com.globo.starwars.model.People;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.text.MessageFormat;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 @Service
-public class StarWarsService {
-
-    @Autowired
-    private RestTemplate restTemplate;
+public class MovieService {
 
     private static final String BASE_URL = "https://swapi.dev/api/";
     private static final String URL_MOVIES = BASE_URL + "films/";
     private static final String URL_MOVIE_BY_ID = URL_MOVIES + "{0}/";
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Cacheable(cacheNames = "movies")
     public List<Movie> getAllMovies() {
-        return restTemplate.getForObject(URL_MOVIES, MovieWrapper.class).getMovies();
+        List<Movie> movies = restTemplate.getForObject(URL_MOVIES, MovieWrapper.class).getMovies();
+
+        for (Movie movie : movies) {
+            movie.setCast(createListCharactersForMovie(movie));
+        }
+
+        return movies;
     }
 
+    @Cacheable(cacheNames = "movie", key = "#id")
     public Movie getMovieById(Long id) throws JsonProcessingException {
         Movie movie = restTemplate.getForObject(MessageFormat.format(URL_MOVIE_BY_ID, id), Movie.class);
         movie.setId(id);
